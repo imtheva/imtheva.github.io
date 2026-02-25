d2001ba539c54cad322b6720884156240ae6ded2
imtheva <86908842+imtheva@users.noreply.github.com>
2026-02-25 14:29:54 +0000

    Deploying to gh-pages from @ imtheva/imtheva.github.io@e05177e2504355a23a3caae153cdeb49852f3a01 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -65,13 +65,6 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/gallery/";
               },
-            },{id: "dropdown-useful-links",
-              title: "Useful Links",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/useful-links/";
-              },
             },{id: "post-winter-storms-how-they-form-why-they-re-dangerous-and-what-they-mean-for-agriculture",
         
           title: 'Winter Storms: How They Form, Why They’re Dangerous, and What They Mean for... <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
